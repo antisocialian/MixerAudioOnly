af8d3489b7af483e4f8f0ccc11cffcec1d1d258a
antisocialian <antisocialian@gmail.com>
2019-09-26 10:50:21 -0400

    Create MixerAudioOnly.js
    initial

---
+++ MixerAudioOnly.js
@@ -0,0 +1,120 @@
+// ==UserScript==
+// @name         Mixer Audio Only
+// @namespace    https://github.com/antisocialian
+// @version      0.3
+// @description  Set streams to audio-only depending on the state of a checkbox/cookie
+// @author       antisocialian
+// @match        *mixer.com/*
+// @homepageURL  https://github.com/antisocialian/MixerAudioOnly
+// @supportURL   https://github.com/antisocialian/MixerAudioOnly/issues
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //add the checkbox along the top menu on the page and add hook to its onClick() event
+    document.querySelector("nav button").insertAdjacentHTML("afterend", "<label _ngcontent-c5 class='nav-link'><input type='checkbox' id='chkaudioOnly' value='audioOnly'> Audio-Only</label>");
+    var m = document.getElementById("chkaudioOnly");
+    m.addEventListener('click', tglAO, false);
+
+    //initialize global variables
+    var audioOnly = false;
+    var intervalvariable;
+
+    //get the cookie that stores true/false for the page
+    checkCookie();
+
+    //hook into the window's load event to check that the video is in the correct state. doing this as an interval DOES mean it is running constantly while the checkbox is checked,
+    //but ALSO means it will catch when the channel hosts or goes offline & online again. This was the only workaround I could get to catch whent he channel had hosted someone else(or went offline&online)
+    window.addEventListener('load', function() {
+        intervalvariable = setInterval(cAO, ((Math.random()) + 3) *1000);
+    }, false);
+
+    //this is the event for the checkbox onClick() hook
+    //it should set the global variable to the state of the checkbox, then set the cookie, then redo the audio-only button, and finally stop/start the interval function again
+    function tglAO() {
+        //var tmpAO = m.checked;
+        audioOnly = m.checked;
+        setCookie("audioOnly", audioOnly, 30);
+        cAO();
+        if (m.checked == false) {
+            clearInterval(intervalvariable);
+        } else {
+            intervalvariable = setInterval(cAO, ((Math.random()) + 3) *1000);
+        }
+    }
+
+    //this is the function to check/click the audio-only button
+    function cAO(){
+        //get the list of elements with the correct class.
+        //**NOTE** this may need to be updated, should the site change the layout of the page
+        var x = document.getElementsByClassName("_2YmB_I5OliPyB7_rs748W3 _1kIlUXtgizhBW5Drjbvqmm");
+        var i,j;
+
+        for (i = 0; i < x.length; i++) {
+            //since the button element uses an aria-label and not a name/id we have to check the attributes of each element to find the audio-only button
+            for (j = 0; j < x[i].attributes.length; j++) {
+                if (x[i].attributes[j].value == "Audio Only") {
+                    //comparing the inner <div> HTML of the button to check the state of the audio only.
+                    //this was the only waay I could find to get the state of the button from the page so that we could properly click the button
+                    //the problem is that the button looks like a checkbox, but is in fact a button that changes the icon of a checkbox depending
+                    //on whether it has been clicked on/off
+                    var p = x[i].innerHTML;
+                    var r = p.includes("check_box_outline_blank");
+                    if (r == true) {
+                        if (audioOnly == true) {
+                            m.checked = true;
+                            x[i].click();
+                        }
+                    } else {
+                        if (audioOnly == false) {
+                            m.checked = false;
+                            x[i].click();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //copied from w3schools.com/js/js_cookies.asp
+    //i didn't make this function
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires;
+    }
+
+    //copied from w3schools.com/js/js_cookies.asp
+    //i didn't make this function
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    //check the cookie and set the global variable & checkbox accordingly
+    function checkCookie() {
+        var tmpCookie = getCookie("audioOnly");
+        if (tmpCookie == 'true') {
+            audioOnly = true;
+            m.checked = true;
+        } else {
+            audioOnly = false;
+            m.checked = false;
+        }
+    }
+}
+)();
